21723f923499910d33d18952986e74c0b0629790
webclass <upmorimoto.class@gmail.com>
2023-10-24 08:47:26 +0900

    shinseikan

---
+++ Samples/shinseikan/shinseikan_sam2/js/main.js
@@ -0,0 +1,28 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+  // classが"sen"の要素を取得
+  const senElements = document.querySelectorAll('.sen');
+
+  // アニメーションを実行する関数
+  function animateElements() {
+      senElements.forEach((senElement) => {
+          // ランダムな座標を生成
+          const randomX = Math.random() * (window.innerWidth - 0); // 400はセクションの幅
+          const randomY = Math.random() * (window.innerHeight - 0); // 200はセクションの高さ
+
+          // 要素をランダムな座標に配置
+          senElement.style.left = `${randomX}px`;
+          senElement.style.top = `${randomY}px`;
+      });
+
+      // アニメーションを再帰的に実行
+      requestAnimationFrame(animateElements);
+  }
+
+  // アニメーションの更新間隔を調整
+  const animationInterval = 1000 / 3; // 5フレーム/秒に設定（5秒ごとに要素がランダムに配置）
+
+  // 初回のアニメーション開始
+  setTimeout(function() {
+      animateElements();
+  }, animationInterval);
+});
