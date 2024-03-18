baeee9aedb856a51e0aa25c46d75b8bf8116f1aa
upmorimoto <116855625+upmorimoto@users.noreply.github.com>
2024-03-18 15:10:47 +0900

    3/18

---
+++ Samples/web_class/flower farm & shop/script.js
@@ -0,0 +1,31 @@
+$(".openbtn").click(function () {//ボタンがクリックされたら
+	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+});
+
+const $header=document.getElementById('header');
+window.addEventListener('scroll' , ()=>{
+    let scroll=window.scrollY;
+    if (100<scrollY){
+        $header.classList.add('on');
+    }else{
+        $header.classList.remove('on');
+    }
+});
+
+// ページ内スクロール
+$('a[href^="#"]').click(function () {
+    const speed = 1000;
+    let href = $(this).attr("href");
+    let target = $(href == "#" || href == "" ? "html" : href);
+    let position = target.offset().top;
+    $("body,html").animate({ scrollTop: position }, speed, "swing");
+    return false;
+});
+
+
