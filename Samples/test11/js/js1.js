2a56a39e8b6abe0fda3ef32255cb24388ca2713e
upmorimoto <116855625+upmorimoto@users.noreply.github.com>
2024-03-29 17:54:27 +0900

    323common

---
+++ Samples/test11/js/js1.js
@@ -0,0 +1,16 @@
+const smoothScrollTrigger = document.querySelectorAll('a[href^="#"]');
+  for (let i = 0; i < smoothScrollTrigger.length; i++){
+    smoothScrollTrigger[i].addEventListener('click', (e) => {
+      e.preventDefault();
+      let href = smoothScrollTrigger[i].getAttribute('href');
+       let targetElement = document.getElementById(href.replace('#', ''));
+      const rect = targetElement.getBoundingClientRect().top;
+      const offset = window.pageYOffset;
+      const gap = 60;
+      const target = rect + offset - gap;
+      window.scrollTo({
+        top: target,
+        behavior: 'smooth',
+      });
+    });
+  }
